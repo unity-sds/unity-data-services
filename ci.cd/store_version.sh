79dee0a86c25eca557640ef2e8c0c358a9251dee
Wai Phyo <wai.phyo@jpl.nasa.gov>
2022-07-12 13:22:02 -0700

    fix: docker github action

--- ci.cd/store_version.sh
+++ ci.cd/store_version.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-software_version=`python3 ${project_root_dir}/setup.py --version`
+software_version=`python3 ${GITHUB_WORKSPACE}/setup.py --version`
 echo $software_version
 echo "software_version=${software_version}" >> ${GITHUB_ENV}
