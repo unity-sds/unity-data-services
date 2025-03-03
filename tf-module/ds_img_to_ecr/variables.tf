cb7e8badb6ca0f1d81e253d4d04b9a89bf580e9b
Wai Phyo <wai.phyo@jpl.nasa.gov>
2025-03-03 11:51:12 -0800

    feat: using ECR pull through

--- tf-module/ds_img_to_ecr/variables.tf
+++ tf-module/ds_img_to_ecr/variables.tf
@@ -1,49 +1,19 @@
 variable "prefix" {
   type = string
 }
-variable "aws_region" {
-  type    = string
-  default = "us-west-2"
-}
-variable "tags" {
-  description = "Tags to be applied to Cumulus resources that support tags"
-  type        = map(string)
-  default     = {}
-}
 
-variable "github_image_url" {
-  description = "The full URL of the public Docker image on GitHub (e.g., ghcr.io/user/repo)"
-  default = "ghcr.io/unity-sds/unity-data-services"
-  type        = string
-}
-
-variable "image_tag" {
-  description = "The tag of the image to pull from GitHub"
-  type        = string
-}
-
-variable "ecr_repo_name" {
-  description = "The name of the ECR repository"
-  default = "unity-data-services"
-  type        = string
-}
-
-variable "ami_id" {
-  description = "The AMI ID for the EC2 instance"
-  type        = string
-}
-
-variable "instance_type" {
-  description = "The type of EC2 instance"
-  default = "t3.medium"
-  type        = string
-}
-
-variable "cumulus_lambda_vpc_id" {
-  description = "VPC ID."
+variable "ecr_github_username" {
   type = string
+  description = "Github username for ECR Pull Through"
+
 }
-variable "cumulus_subnet_id" {
-  description = "subnet group ID where a new EC2 will be placed"
+variable "ecr_github_token" {
   type = string
+  description = "Github Fine-grained personal access token for ECR Pull Through that has access to https://github.com/unity-sds/unity-data-services"
+}
+
+variable "uds_repo_name" {
+  type = string
+  default = "unity-sds/unity-data-services"
+  description = "Github group and repo"
 }
