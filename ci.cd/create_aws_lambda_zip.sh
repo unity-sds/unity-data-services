5026be7189d3d582a0c630fcc6b9365d920d7b0b
Wai Phyo <wai.phyo@jpl.nasa.gov>
2025-03-17 11:32:33 -0500

    feat: add new zip files

--- ci.cd/create_aws_lambda_zip.sh
+++ ci.cd/create_aws_lambda_zip.sh
@@ -5,10 +5,15 @@ apt-get update -y && apt-get install zip -y
 ZIP_NAME='cumulus_lambda_functions_deployment.zip'
 TERRAFORM_ZIP_NAME='terraform_cumulus_lambda_functions_deployment.zip'
 TERRAFORM_MARKETPLACE_ZIP_NAME='terraform_marketplace_deployment.zip'
+TERRAFORM_ECR_ZIP_NAME='terraform_ecr_deployment.zip'
+TERRAFORM_STAC_BR_ZIP_NAME='terraform_stac_br_deployment.zip'
+
 project_root_dir=${GITHUB_WORKSPACE}
 zip_file="${project_root_dir}/$ZIP_NAME" ; # save the result file in current working directory
 terraform_zip_file="${project_root_dir}/$TERRAFORM_ZIP_NAME" ; # save the result file in current working directory
 terraform_marketplace_zip_file="${project_root_dir}/$TERRAFORM_MARKETPLACE_ZIP_NAME" ; # save the result file in current working directory
+terraform_ecr_zip_file="${project_root_dir}/$TERRAFORM_ECR_ZIP_NAME" ; # save the result file in current working directory
+terraform_stac_br_zip_file="${project_root_dir}/$TERRAFORM_STAC_BR_ZIP_NAME" ; # save the result file in current working directory
 
 source_dir=`python3 -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])'`
 
@@ -26,6 +31,12 @@ zip -9 ${terraform_zip_file} * **/*
 
 cd $project_root_dir/tf-module/marketplace
 zip -9 ${terraform_marketplace_zip_file} * **/*
+
+cd $project_root_dir/tf-module/ds_img_to_ecr
+zip -9 ${terraform_ecr_zip_file} * **/*
+
+cd $project_root_dir/tf-module/stac_browser
+zip -9 ${terraform_stac_br_zip_file} * **/*
 
 # github.job
 github_branch=${GITHUB_REF##*/}
