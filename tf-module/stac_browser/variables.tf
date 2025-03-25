d357a00748ca24351606aeeafe403f422d0b6cb5
wphyojpl <38299756+wphyojpl@users.noreply.github.com>
2025-03-25 11:55:26 -0700

    feat: stac browser from terraform (#547)
    * feat: terraform script for stac-browser with ALB
    
    * feat: add output
    
    * feat: add sample setting files
    
    * fix: remove unnecessary security permissions + ec2 extra permission
    
    * fix: passing cidr blocks from variable
    
    * chore: restructure actions file for easier readability
    
    * feat: add new zip files
    
    * fix: add 2 new zip files
    
    * fix: add missing variables
    
    * fix: assumed that docker is installed and running on AMI (ECS images)
    
    * fix: require different secret to avoid 7 days in recovery
    
    * fix: add tags from env

---
+++ tf-module/stac_browser/variables.tf
@@ -0,0 +1,56 @@
+variable "prefix" {
+  type = string
+}
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
+variable "tags" {
+  description = "Tags to be applied to Cumulus resources that support tags"
+  type        = map(string)
+  default     = {}
+}
+
+variable "cumulus_lambda_vpc_id" {
+  description = "VPC ID."
+  type = string
+}
+
+variable "subnet_ids" {
+  description = "subnet IDs for Lambdas"
+  type        = list(string)
+  default     = null
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  default = "t3.medium"
+  type        = string
+}
+
+
+variable "github_image_url" {
+  description = "The full URL of the public Docker image on GitHub (e.g., ghcr.io/user/repo)"
+  default = "ghcr.io/unity-sds/unity-data-services"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "The tag of the image to pull from GitHub"
+  type        = string
+}
+
+variable "shared_services_ec2_subnet_cidr" {
+  type = string
+  description = "CIDR of the Subnet where Shared Services EC2 resides. This is to allow connections from Shared services to ALB. Example: 10.52.0.0/16"
+}
+
+variable "alb_subnet_cidr" {
+  type        = string
+  description = "CIDR of the Subnet where ALB resides. This is to allow connections from ALB to Stac Browser EC2. Example: 10.52.0.0/16"
+}
