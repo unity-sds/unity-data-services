70392578c5a05247c675d2f3c7d05470c19324e1
wphyojpl <wai.phyo@jpl.nasa.gov>
2025-03-25 18:55:38 +0000

    chore: update version + change log

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
 
 setup(
     name="cumulus_lambda_functions",
-    version="9.9.0",
+    version="9.10.0",
     packages=find_packages(),
     install_requires=install_requires,
     package_data={
