cf4e44c491bc4fcf4dbb4050b7c8f3a8f22d26b4
Wai Phyo <wai.phyo@jpl.nasa.gov>
2026-06-22 12:34:10 -0700

    fix: target collection in all records

--- cumulus_lambda_functions/daac_archiver/ddb_mws/catalia_status_db.py
+++ cumulus_lambda_functions/daac_archiver/ddb_mws/catalia_status_db.py
@@ -6,6 +6,7 @@ from mdps_ds_lib.lib.aws.no_sql_factory import NoSqlFactory
 class CataliaStatusDb:
     identifier = 'identifier'
     collection = 'collection'
+    target_collection = 'target_collection'
     name_str = 'name'
     status = 'status'
     error_code = 'errorCode'
@@ -28,7 +29,7 @@ class CataliaStatusDb:
         results = self.__ddb.get(identifier, secondary_key=None)
         return results
 
-    def add(self, identifier: str, collection: dict, name_str: str, status: str, datetime_str: str, error_code: str=None, error_message: str=None, href_str: str=None):
+    def add(self, identifier: str, collection: dict, name_str: str, status: str, datetime_str: str, error_code: str=None, error_message: str=None, href_str: str=None, target_collection: str=None):
         item1 = {
             self.name_str: name_str,
             self.collection: collection,
@@ -36,6 +37,7 @@ class CataliaStatusDb:
             self.error_code: error_code,
             self.error_message: error_message,
             self.href_str: href_str,
+            self.target_collection: target_collection
         }
         item1 = {k: v for k, v in item1.items() if v is not None}
         self.__ddb.add(identifier, datetime_str, item1, replace=False)
